9cefc49cbf4a68815efdff9d68156807e44cb970
TrinhBaoDuy <2051050075duy@ou.edu.vn>
2023-09-12 02:55:46 +0700

    update

--- Clinnic/target/generated-sources/annotations/com/owen/pojo/Service_.java
+++ Clinnic/target/generated-sources/annotations/com/owen/pojo/Service_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SetAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-12T02:27:25")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2023-09-12T02:53:29")
 @StaticMetamodel(Service.class)
 public class Service_ { 
 
